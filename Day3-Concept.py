f9a63189bf7cfc6d77b5d16ded9f77900a2c772a
Rohit B <rohetbiswash@gmail.com>
2026-01-01 15:31:01 +0530

    Day 3

--- Day3-Concept.py
+++ Day3-Concept.py
@@ -172,9 +172,9 @@ print(dict["sub"]) #returns value according to key but if key is incorrect it th
 print(dict.get("sub")) #returns value according to key  but if key is incorrect it throws None
 
 
-# Sets -> An unordered collection of unique elements.
+# Sets -> An unordered collection of unique elements(no duplicate values).
 # Since unordered collections we can't do indexing or slicing
-# Sets are mutable -> add or remove elements
+# Sets are mutable, but not index-mutable -> Only supports add or remove elements
 # Set elements must be immutable, so only immutable data types—such as strings, numbers, and tuples—can be stored in a set.
 
 set1 = {1,2,3,2,4,5,2}
@@ -184,6 +184,7 @@ print(len(set1))
 
 # Adding new values
 set1.add(100)
+set1.remove(3)
 print(set1)
 
 # Creating empty sets
